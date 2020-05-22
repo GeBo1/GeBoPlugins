7a20563f9fb01f0417bf7df18781c60bc7eac8f5
GeBo1 <57293591+GettaBobo@users.noreply.github.com>
2020-05-22 18:24:28 +0000

    KK_GameDressForSuccess: right click on clothing type changes clothes and keeps set to auto

--- KK_GameDressForSuccess/GameDressForSuccess.Hooks.cs
+++ KK_GameDressForSuccess/GameDressForSuccess.Hooks.cs
@@ -1,8 +1,14 @@
-﻿using ADV.Commands.Base;
+﻿using System.Collections.Generic;
+using System.Linq;
 using ADV.Commands.Chara;
 using ADV.Commands.Game;
 using BepInEx.Logging;
+using ChaCustom;
 using HarmonyLib;
+using Manager;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Text = ADV.Commands.Base.Text;
 
 namespace GameDressForSuccessPlugin
 {
@@ -10,6 +16,10 @@ namespace GameDressForSuccessPlugin
     {
         internal class Hooks
         {
+            private const string TogglePrefix = "tglCoorde";
+            private const string AutoToggleName = TogglePrefix + "00";
+
+            
             internal static ManualLogSource Logger => Instance?.Logger;
 
             [HarmonyPostfix]
@@ -33,16 +43,59 @@ namespace GameDressForSuccessPlugin
             [HarmonyPatch(typeof(Coordinate), "Do")]
             internal static void CoordinateDoPostfix(Coordinate __instance)
             {
-                if (!Enabled.Value) return;
+                if (Instance == null || !Enabled.Value || Instance._monitoringChange) return;
 
-                var type =
-                    (ChaFileDefine.CoordinateType?)AccessTools.Field(__instance.GetType(), "type")
-                        ?.GetValue(__instance);
-                if (type.HasValue)
-                {
-                    Instance?.DressPlayer(type.Value);
-                }
+
+                var typeField = Traverse.Create(__instance).Field("type");
+
+                if (!typeField.FieldExists()) return;
+
+                Instance?.DressPlayer(typeField.GetValue<ChaFileDefine.CoordinateType>());
             }
+
+            #region Right Click Clothing Support
+
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(Toggle), nameof(Toggle.OnPointerClick))]
+            internal static void ToggleOnPointerClickPrefix(Toggle __instance,
+                ref PointerEventData eventData, out Toggle __state)
+            {
+                __state = null;
+                if (!Enabled.Value ||
+                    eventData.button != PointerEventData.InputButton.Right ||
+                    !__instance.name.StartsWith(TogglePrefix))
+                {
+                    return;
+                }
+
+                // everything after this point will fire the default left-click event
+                eventData.button = PointerEventData.InputButton.Left;
+
+                
+                if (__instance.name == AutoToggleName) return;
+
+                var autoEnabled = (CustomBase.IsInstance() &&
+                                   Singleton<CustomBase>.Instance.autoClothesState) ||
+                                  (Game.IsInstance() &&
+                                   Singleton<Game>.Instance.Player.changeClothesType < 0);
+
+                if (!autoEnabled) return;
+
+                var togglesField = Traverse.Create(__instance.group).Field("m_Toggles");
+
+                __state = togglesField.FieldExists()
+                    ? togglesField.GetValue<List<Toggle>>().FirstOrDefault(t => t.name == AutoToggleName)
+                    : null;
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(Toggle), nameof(Toggle.OnPointerClick))]
+            internal static void ToggleOnPointerClickPostfix(Toggle __state)
+            {
+                __state?.OnSubmit(null);
+            }
+
+            #endregion
 
             /*
             [HarmonyPrefix]

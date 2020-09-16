8ff75a53511a914aefd1b048b19baeeab1b73476
GeBo1 <57293591+GettaBobo@users.noreply.github.com>
2020-09-16 21:40:29 +0000

    new plugin: HS2_GameWhoIsThere

---
+++ src/GameWhoIsThere/HS2_GameWhoIsThere/GameWhoIsThere.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Linq;
+using AIChara;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace GameWhoIsTherePlugin
+{
+    [BepInPlugin(GUID, PluginName, Version)]
+    public class GameWhoIsThere : BaseUnityPlugin
+    {
+        public const string GUID = "com.gebo.bepinex.whoisthere";
+        public const string PluginName = "Who Is There?";
+        public const string Version = "1.0";
+
+        public static GameWhoIsThere Instance;
+        internal static new ManualLogSource Logger;
+        private readonly ChaFileControl[] _chaFileControls = {null, null};
+
+        private readonly Text[] _labels = {null, null};
+        private bool _busy;
+        private bool _keyIdle = true;
+
+        public static ConfigEntry<bool> Enabled { get; private set; }
+        public static ConfigEntry<KeyboardShortcut> ShowWhoIsThereShortcut { get; private set; }
+
+        internal bool Active => InMyRoom && _labels.Any(x => x != null);
+        internal bool InMyRoom { get; private set; }
+
+        internal void Main()
+        {
+            Instance = this;
+            Logger = Logger ?? base.Logger;
+
+            Enabled = Config.Bind("Settings", "Enabled", true, "Whether the plugin is enabled");
+            ShowWhoIsThereShortcut = Config.Bind("Keyboard Shortcuts", "Toggle showing who is in room",
+                new KeyboardShortcut(KeyCode.Slash),
+                "Display name of character in surprise events");
+
+            Reset();
+            Hooks.SetupHooks();
+            SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+            SceneManager.sceneUnloaded += SceneManager_sceneUnloaded;
+        }
+
+
+        private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+        {
+            if (arg0.name == "MyRoom" && Enabled.Value) InMyRoom = true;
+        }
+
+
+        private void SceneManager_sceneUnloaded(Scene arg0)
+        {
+            if (!InMyRoom) return;
+            var wasActive = Active;
+            InMyRoom = false;
+            if (!Active && wasActive) Reset();
+        }
+
+        internal void Update()
+        {
+            if (!Active || _busy || _labels[0] == null) return;
+
+            var isPressed = ShowWhoIsThereShortcut.Value.IsPressed();
+            if (_keyIdle)
+            {
+                if (!isPressed) return;
+                _keyIdle = false;
+                StartCoroutine(UpdateLabels());
+            }
+            else
+            {
+                if (!isPressed) _keyIdle = true;
+            }
+        }
+
+
+        private IEnumerator UpdateLabels()
+        {
+            if (_busy) yield break;
+            yield return null;
+            _busy = true;
+            try
+            {
+                for (var i = 0; i < _labels.Length; i++)
+                {
+                    if (_labels[i] == null || _chaFileControls[i] == null) continue;
+                    _labels[i].text = _labels[i].text == "???" ? $"({_chaFileControls[i].parameter.fullname})" : "???";
+                }
+
+                yield return null;
+            }
+            finally
+            {
+                _busy = false;
+            }
+        }
+
+        internal void ConfigureDisplay(int index, Text label, ChaFileControl who)
+        {
+            _labels[index] = label;
+            _chaFileControls[index] = who;
+        }
+
+        internal void Reset()
+        {
+            for (var i = 0; i < _labels.Length; i++)
+            {
+                _labels[i] = null;
+                _chaFileControls[i] = null;
+            }
+        }
+    }
+}
---
+++ src/GameWhoIsThere/HS2_GameWhoIsThere/Hooks.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using AIChara;
+using BepInEx.Logging;
+using HarmonyLib;
+using HS2;
+using UnityEngine.UI;
+
+namespace GameWhoIsTherePlugin
+{
+    internal class Hooks
+    {
+        private static bool _inMapSelecCursorEnter;
+        private static int _inMapSelecCursorEnterIndex;
+
+        private static readonly List<Text> InMapSelecCursorLabels = new List<Text> {null, null};
+        private static readonly List<ChaFileControl> InMapSeleChaFileControls = new List<ChaFileControl> {null, null};
+        internal static ManualLogSource Logger => GameWhoIsThere.Logger;
+
+        internal static Harmony SetupHooks()
+        {
+            return Harmony.CreateAndPatchAll(typeof(Hooks));
+        }
+
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(MapSelectUI), "MapSelecCursorEnter")]
+        internal static void MapSelecCursorEnterPrefix(MapSelectUI __instance)
+        {
+            if (!GameWhoIsThere.Instance.InMyRoom) return;
+            _inMapSelecCursorEnter = true;
+            GameWhoIsThere.Instance.Reset();
+            _inMapSelecCursorEnterIndex = 0;
+            var i = 0;
+            foreach (var uiName in new[] {"firstCharaThumbnailUI", "secondCharaThumbnailUI"})
+            {
+                try
+                {
+                    InMapSelecCursorLabels[i] = Traverse.Create(__instance)?.Field(uiName)
+                        ?.Field<Text>("text")?.Value;
+                }
+                catch
+                {
+                    InMapSelecCursorLabels[i] = null;
+                }
+
+                i++;
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(ChaFileControl), nameof(ChaFileControl.LoadCharaFile), typeof(string), typeof(byte),
+            typeof(bool), typeof(bool))]
+        internal static void ChaFileControl_LoadCharaFile_Postfix(ChaFileControl __instance)
+        {
+            if (!_inMapSelecCursorEnter || __instance == null) return;
+            InMapSeleChaFileControls[_inMapSelecCursorEnterIndex] = __instance;
+            _inMapSelecCursorEnterIndex++;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(MapSelectUI), "MapSelecCursorEnter")]
+        internal static void MapSelecCursorEnterPostfix()
+        {
+            if (!_inMapSelecCursorEnter) return;
+            _inMapSelecCursorEnter = false;
+            for (var i = 0; i < 2; i++)
+            {
+                var label = InMapSelecCursorLabels[i];
+                var chaFileControl = InMapSeleChaFileControls[i];
+                InMapSelecCursorLabels[i] = null;
+                InMapSelecCursorLabels[i] = null;
+                if (label == null || chaFileControl == null || label.text != "???") continue;
+                GameWhoIsThere.Instance.ConfigureDisplay(i, label, chaFileControl);
+            }
+        }
+    }
+}
